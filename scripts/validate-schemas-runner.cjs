1b8f245bfe1fe3adbdf481e00726d120edeabda7
sbporter <sethbporter@gmail.com>
2025-03-05 22:46:49 -0700

    clean up validate-schemas

--- scripts/validate-schemas-runner.cjs
+++
@@ -1,5 +0,0 @@
-// Simple runner for our CommonJS validation script
-console.log('Running schema validation...');
-
-// Just require and run the CommonJS validation script directly
-require('./validate-schemas.cjs'); 
